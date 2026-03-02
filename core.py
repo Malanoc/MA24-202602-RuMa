c65371a5764498bd8180686f6c08b99a651b69a1
Malanoc <tm.crz1@gmail.com>
2026-03-02 15:06:42 +0100

    bug fixes

--- core.py
+++ core.py
@@ -19,8 +19,12 @@ DIRECTIONS = [
     (1, -1),  (1, 0),  (1, 1)
 ]
 
-#  Crée un plateau vide 8x8, puis place les 4 pions de départ.
+
 def creer_plateau():
+    """
+    Crée un plateau vide 8x8
+    puis place les 4 pions de départ.
+    """
 
     plateau = []
 
@@ -43,16 +47,22 @@ def creer_plateau():
 
     return plateau
 
-# Vérifie si une position est bien à l'intérieur du plateau.
-def position_valide(ligne, colonne):
 
+def position_valide(ligne, colonne):
+    """
+    Vérifie si une position est bien à l'intérieur du plateau.
+    """
     if ligne >= 0 and ligne < BOARD_SIZE and colonne >= 0 and colonne < BOARD_SIZE:
         return True
     else:
         return False
 
-# Retourne la liste des pions qui seront retournés, si le joueur joue à la position donnée.
+
 def pions_a_retourner(plateau, ligne, colonne, joueur):
+    """
+    Retourne la liste des pions qui seront retournés
+    si le joueur joue à la position donnée.
+    """
 
     liste_pions = []
 
@@ -86,8 +96,11 @@ def pions_a_retourner(plateau, ligne, colonne, joueur):
 
     return liste_pions
 
-#  Retourne la liste des coups possibles pour le joueur.
+
 def coups_valides(plateau, joueur):
+    """
+    Retourne la liste des coups possibles pour le joueur.
+    """
 
     liste_coups = []
 
@@ -103,8 +116,11 @@ def coups_valides(plateau, joueur):
 
     return liste_coups
 
-# Applique un coup sur le plateau.
+
 def jouer_coup(plateau, ligne, colonne, joueur):
+    """
+    Applique un coup sur le plateau.
+    """
 
     pions = pions_a_retourner(plateau, ligne, colonne, joueur)
 
@@ -120,142 +136,11 @@ def jouer_coup(plateau, ligne, colonne, joueur):
 
     return True
 
-#   Compte le nombre de pions noirs et blancs.
+
 def calculer_score(plateau):
-
-    score_noir = 0
-    score_blanc = 0
-
-    for ligne in plateau:
-        for case in ligne:
-
-            if case == PION_NOIR:
-                score_noir = score_noir + 1
-
-            if case == PION_BLANC:
-                score_blanc = score_blanc + 1
-
-    return score_noir, score_blanc
-# core.py
-
-# Taille du plateau (8 lignes et 8 colonnes)
-BOARD_SIZE = 8
-
-# Valeurs utilisées dans le tableau
-CASE_VIDE = 0
-PION_NOIR = 1
-PION_BLANC = -1
-
-# Les 8 directions possibles autour d’une case
-DIRECTIONS = [
-    (-1, -1), (-1, 0), (-1, 1),
-    (0, -1),           (0, 1),
-    (1, -1),  (1, 0),  (1, 1)
-]
-
-#  Crée un plateau vide 8x8, puis place les 4 pions de départ.
-def creer_plateau():
-
-    plateau = []
-
-    # Création des 8 lignes
-    for ligne in range(BOARD_SIZE):
-
-        nouvelle_ligne = []
-
-        # Création des 8 colonnes
-        for colonne in range(BOARD_SIZE):
-            nouvelle_ligne.append(CASE_VIDE)
-
-        plateau.append(nouvelle_ligne)
-
-    # Placement des 4 pions initiaux au centre
-    plateau[3][3] = PION_BLANC
-    plateau[4][4] = PION_BLANC
-    plateau[3][4] = PION_NOIR
-    plateau[4][3] = PION_NOIR
-
-    return plateau
-
-# Vérifie si une position est bien à l'intérieur du plateau.
-def position_valide(ligne, colonne):
-
-    if ligne >= 0 and ligne < BOARD_SIZE and colonne >= 0 and colonne < BOARD_SIZE:
-        return True
-    else:
-        return False
-
-# Retourne la liste des pions qui seront retournés, si le joueur joue à la position donnée.
-def pions_a_retourner(plateau, ligne, colonne, joueur):
-
-    liste_pions = []
-
-    # On teste les 8 directions
-    for direction in DIRECTIONS:
-
-        delta_ligne = direction[0]
-        delta_colonne = direction[1]
-
-        ligne_courante = ligne + delta_ligne
-        colonne_courante = colonne + delta_colonne
-
-        pions_temp = []
-
-        # On avance tant qu'on trouve des pions adverses
-        while position_valide(ligne_courante, colonne_courante) and \
-              plateau[ligne_courante][colonne_courante] == -joueur:
-
-            pions_temp.append((ligne_courante, colonne_courante))
-
-            ligne_courante = ligne_courante + delta_ligne
-            colonne_courante = colonne_courante + delta_colonne
-
-        # Si on tombe sur un pion du joueur, la capture est valide
-        if position_valide(ligne_courante, colonne_courante) and \
-           plateau[ligne_courante][colonne_courante] == joueur and \
-           len(pions_temp) > 0:
-
-            for pion in pions_temp:
-                liste_pions.append(pion)
-
-    return liste_pions
-
-#  Retourne la liste des coups possibles pour le joueur.
-def coups_valides(plateau, joueur):
-
-    liste_coups = []
-
-    for ligne in range(BOARD_SIZE):
-        for colonne in range(BOARD_SIZE):
-
-            if plateau[ligne][colonne] == CASE_VIDE:
-
-                pions = pions_a_retourner(plateau, ligne, colonne, joueur)
-
-                if len(pions) > 0:
-                    liste_coups.append((ligne, colonne))
-
-    return liste_coups
-
-# Applique un coup sur le plateau.
-def jouer_coup(plateau, ligne, colonne, joueur):
-
-    pions = pions_a_retourner(plateau, ligne, colonne, joueur)
-
-    if len(pions) == 0:
-        return False
-
-    plateau[ligne][colonne] = joueur
-
-    for position in pions:
-        l = position[0]
-        c = position[1]
-        plateau[l][c] = joueur
-
-    return True
-
-#   Compte le nombre de pions noirs et blancs.
-def calculer_score(plateau):
+    """
+    Compte le nombre de pions noirs et blancs.
+    """
 
     score_noir = 0
     score_blanc = 0
--- gfx.py
+++ gfx.py
@@ -17,8 +17,12 @@ COULEUR_NOIR = (30, 30, 30)
 COULEUR_BLANC = (245, 245, 245)
 COULEUR_LIGNES = (0, 0, 0)
 
-#Initialise la fenêtre pygame et retourne l'écran ainsi que la police utilisée.
+
 def initialiser_fenetre():
+    """
+    Initialise la fenêtre pygame et retourne
+    l'écran ainsi que la police utilisée.
+    """
 
     pygame.init()
 
@@ -34,8 +38,11 @@ def initialiser_fenetre():
 
     return ecran, police
 
-#Dessine le plateau et les pions.
+
 def dessiner_plateau(ecran, plateau):
+    """
+    Dessine le plateau et les pions.
+    """
 
     # Remplir le fond avec la couleur verte
     ecran.fill(COULEUR_PLATEAU)
@@ -71,8 +78,11 @@ def dessiner_plateau(ecran, plateau):
                     couleur_pion
                 )
 
-#  Dessine un pion avec un rendu lisse (anti-aliasing).
+
 def dessiner_pion(ecran, centre, rayon, couleur):
+    """
+    Dessine un pion avec un rendu lisse (anti-aliasing).
+    """
 
     centre_x = centre[0]
     centre_y = centre[1]
@@ -83,110 +93,11 @@ def dessiner_pion(ecran, centre, rayon, couleur):
     # Contour lisse
     pygame.gfxdraw.aacircle(ecran, centre_x, centre_y, rayon, couleur)
 
-#  Affiche le score et indique le joueur actuel.
+
 def dessiner_interface(ecran, police, joueur, plateau):
-
-    score_noir, score_blanc = core.calculer_score(plateau)
-
-    if joueur == core.PION_NOIR:
-        texte_joueur = "Noir"
-    else:
-        texte_joueur = "Blanc"
-
-    texte = (
-        "Noir : " + str(score_noir)
-        + "   Blanc : " + str(score_blanc)
-        + "   Tour : " + texte_joueur
-    )
-
-    surface_texte = police.render(texte, True, COULEUR_LIGNES)
-
-    position_y = core.BOARD_SIZE * TAILLE_CASE + 10
-
-    ecran.blit(surface_texte, (10, position_y))
-# gfx.py
-
-import pygame
-import pygame.gfxdraw
-import core
-
-# Taille d'une case en pixels
-TAILLE_CASE = 90
-
-# Couleurs utilisées
-COULEUR_PLATEAU = (20, 110, 20)
-COULEUR_NOIR = (30, 30, 30)
-COULEUR_BLANC = (245, 245, 245)
-COULEUR_LIGNES = (0, 0, 0)
-
-#Initialise la fenêtre pygame et retourne l'écran ainsi que la police utilisée.
-def initialiser_fenetre():
-
-    pygame.init()
-
-    largeur = core.BOARD_SIZE * TAILLE_CASE
-    hauteur = core.BOARD_SIZE * TAILLE_CASE
-
-    # On ajoute 50 pixels pour la zone d'affichage du score
-    ecran = pygame.display.set_mode((largeur, hauteur + 50))
-
-    pygame.display.set_caption("Othello")
-
-    police = pygame.font.SysFont(None, 32)
-
-    return ecran, police
-
-#Dessine le plateau et les pions.
-def dessiner_plateau(ecran, plateau):
-
-    # Remplir le fond avec la couleur verte
-    ecran.fill(COULEUR_PLATEAU)
-
-    for ligne in range(core.BOARD_SIZE):
-        for colonne in range(core.BOARD_SIZE):
-
-            position_x = colonne * TAILLE_CASE
-            position_y = ligne * TAILLE_CASE
-
-            rectangle = pygame.Rect(
-                position_x,
-                position_y,
-                TAILLE_CASE,
-                TAILLE_CASE
-            )
-
-            # Dessin des lignes du plateau
-            pygame.draw.rect(ecran, COULEUR_LIGNES, rectangle, 1)
-
-            # Dessin des pions si la case n'est pas vide
-            if plateau[ligne][colonne] != core.CASE_VIDE:
-
-                if plateau[ligne][colonne] == core.PION_NOIR:
-                    couleur_pion = COULEUR_NOIR
-                else:
-                    couleur_pion = COULEUR_BLANC
-
-                dessiner_pion(
-                    ecran,
-                    rectangle.center,
-                    TAILLE_CASE // 2 - 10,
-                    couleur_pion
-                )
-
-#  Dessine un pion avec un rendu lisse (anti-aliasing).
-def dessiner_pion(ecran, centre, rayon, couleur):
-
-    centre_x = centre[0]
-    centre_y = centre[1]
-
-    # Cercle rempli
-    pygame.gfxdraw.filled_circle(ecran, centre_x, centre_y, rayon, couleur)
-
-    # Contour lisse
-    pygame.gfxdraw.aacircle(ecran, centre_x, centre_y, rayon, couleur)
-
-#  Affiche le score et indique le joueur actuel.
-def dessiner_interface(ecran, police, joueur, plateau):
+    """
+    Affiche le score et indique le joueur actuel.
+    """
 
     score_noir, score_blanc = core.calculer_score(plateau)
 
